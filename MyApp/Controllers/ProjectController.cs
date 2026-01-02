fca243d7e497c564cb695e29e14b89cbc85d3014
AmandaMajvind <majvindamanda02@gmail.com>
2026-01-02 14:11:47 +0100

    Redigera projekt

--- MyApp/Controllers/ProjectController.cs
+++ MyApp/Controllers/ProjectController.cs
@@ -1,21 +1,19 @@
-﻿using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using MyApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+
 
 namespace MyApp.Controllers
 {
     public class ProjectController : Controller
     {
         private readonly ApplicationDbContext _context;
-        private readonly UserManager<User> _userManager;
 
-        public ProjectController(ApplicationDbContext context, UserManager<User> userManager)
+        public ProjectController(ApplicationDbContext context)
         {
             _context = context;
-            _userManager = userManager;
         }
 
 
@@ -53,68 +51,48 @@ namespace MyApp.Controllers
         [HttpGet]
         public IActionResult Add()
         {
+            ViewBag.Creators = new SelectList(_context.Users, "Id", "Name");
             return View();
         }
 
-        [Authorize] //Man måste vara inloggad för att komma åt Skapa projekt
-        [HttpPost] //Metoden körs när vi klickar Spara i gränssnittet 
-        public async Task<IActionResult> Add(Project project, IFormFile fileToUpload) //Skickar in ett project och zip-filen från formuläret
+        [HttpPost]
+        public IActionResult Add(Project project)
         {
-
-            if (!ModelState.IsValid) //Om något är fel/tomt etc så går vi in i if-satsen
-            {
-                return View(project); //Visa formuläret igen, inget sparas i databasen
-            }
-
-            var user = await _userManager.GetUserAsync(User); //Hämta inloggad användare
-
-            project.CreatorId = user.Id; //Sätter CreatodId på projektet till Id:t på personen som är inloggad
-
-            if(fileToUpload != null && fileToUpload.Length > 0) //Kontrolelrar att en fil har lagt till och att den inte är tom
-            {
-                var fileName = Path.GetFileName(fileToUpload.FileName); //Tar fram filnamnet
-                var filePath = Path.Combine("wwwroot/uploads", fileName); //Skapar en sökväg till servern
-
-                using (var stream = new FileStream(filePath, FileMode.Create)) //Skapar filen på servern
-                {
-                    await fileToUpload.CopyToAsync(stream); //Kopierar innehållet från uppladdade filen och sparar den på servern
-                }
-                project.ZipFile = "/uploads/" + fileName; //Sparar filens !sökväg! (inte filen) i projektet
-            }
-            _context.Projects.Add(project); //Lägger till projektet i Entity Framework
-            await _context.SaveChangesAsync(); //Sparar till databasen, SQL insert 
-
-            ViewBag.SuccessMessage = "Projektet har lagts till!";
-            return View(new Project());
+            _context.Projects.Add(project);
+            _context.SaveChanges();
+            return RedirectToAction("Index", "Home");
         }
 
-        // --- EDIT ---
+        
         [HttpGet]
         public IActionResult Edit(int id)
         {
             var project = _context.Projects.Find(id);
+            ViewBag.Creators = new SelectList(_context.Users, "UserId", "Name", project.CreatorId);
             return View(project);
         }
 
-        [HttpPost]
-        public IActionResult Edit(Project project)
-        {
-            var existingProject = _context.Projects
-                .AsNoTracking()
-                .FirstOrDefault(p => p.ProjectId == project.ProjectId);
-
-            if(existingProject == null)
+       
+            [HttpPost]
+            public IActionResult Edit(Project project)
             {
-                return NotFound();
+                var existingProject = _context.Projects
+                    .AsNoTracking()
+                    .FirstOrDefault(p => p.ProjectId == project.ProjectId);
+
+                if (existingProject == null)
+                    return NotFound();
+
+                //  behåll ägaren – ändras aldrig
+                project.CreatorId = existingProject.CreatorId;
+
+                _context.Projects.Update(project);
+                _context.SaveChanges();
+
+                return RedirectToAction("Index", "Home");
             }
 
-            project.CreatorId = existingProject.CreatorId;
-
-            _context.Projects.Update(project);
-            _context.SaveChanges();
-
-            return RedirectToAction("Index", "Home");
-        }
+        
 
         [HttpGet]
         public IActionResult Delete(int id)
