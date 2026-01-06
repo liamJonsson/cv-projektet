8835b72bb337552419ffb4fa7e43c7da0495b85c
liamJonsson <limme98@gmail.com>
2026-01-06 14:40:25 +0100

    Revert "Uppdaterad profileviewer"
    This reverts commit 64d98ef58817874b7b83cd9a1d58d36c83a80204.

--- MyApp/Controllers/UserController.cs
+++ MyApp/Controllers/UserController.cs
@@ -148,30 +148,21 @@ namespace MyApp.Controllers
             {
                 return NotFound();
             }
-           
+            
             if(User.Identity?.IsAuthenticated == true)
             {
                 var currentUser = await _userManager.GetUserAsync(User);
                 ViewBag.LoggedInUser = currentUser;
             }
+            bool isLoggedOut = !User.Identity.IsAuthenticated;
+            bool isPrivate = userProfile.Visibility == false;
             var loggedInUserId = _userManager.GetUserId(User);
-            bool isOwner = false;
-            if (loggedInUserId != null)
-            {
-                isOwner = int.Parse(loggedInUserId) == userProfile.Id;
-            }
-            string refererUrl = Request.Headers["Referer"].ToString();
-            string currentPath = Request.Path.ToString(); 
 
-            // Om referer innehåller nuvarande sökväg så är det en refresh
-            bool isRefresh = !string.IsNullOrEmpty(refererUrl) && refererUrl.Contains(currentPath);
-            if (!isOwner && !isRefresh)
+            if (loggedInUserId != null && int.Parse(loggedInUserId) != userProfile.Id)
             {
                 userProfile.ProfileViews++;
                 await _context.SaveChangesAsync();
             }
-            bool isLoggedOut = !User.Identity.IsAuthenticated;
-            bool isPrivate = userProfile.Visibility == false;
 
             if (isPrivate && isLoggedOut)
             {
