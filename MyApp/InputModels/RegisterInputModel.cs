e0f4e62b2f7bf04387c33df330ba5a0f789e6a76
Lisaskarf <56032478+Lisaskarf@users.noreply.github.com>
2026-01-08 12:39:47 +0100

    Validering för registrera

--- MyApp/InputModels/RegisterInputModel.cs
+++ MyApp/InputModels/RegisterInputModel.cs
@@ -4,33 +4,41 @@ namespace MyApp.InputModels
 {
     public class RegisterInputModel
     {
-        [Required(ErrorMessage = "Namn krävs")]
+        [Required(ErrorMessage = "Namn krävs.")]
+        [RegularExpression(@"^[a-zA-ZåäöÅÄÖéÉ ]*$", ErrorMessage = "Endast bokstäver tillåtet.")]
         public string Name { get; set; }
 
-        [Required(ErrorMessage = "Telefonnummer krävs")]
+        [Required(ErrorMessage = "Telefonnummer krävs.")]
+        [StringLength(12, ErrorMessage = "Max 12 tecken tillåtet.")]
+        [RegularExpression(@"^[0-9+]*$", ErrorMessage = "Endast siffror tillåtet.")]
         public string PhoneNumber { get; set; }
 
-        [Required(ErrorMessage = "Mailadress krävs")]
+        [Required(ErrorMessage = "Mailadress krävs.")]
         [EmailAddress(ErrorMessage = "Ogiltig mailadress")]
         public string Email { get; set; }
 
-        [Required(ErrorMessage = "Adress krävs")]
+        [Required(ErrorMessage = "Adress krävs.")]
+        [RegularExpression(@"^[a-zA-ZåäöÅÄÖéÉ0-9]*$", ErrorMessage = "Endast bokstäver och siffror tillåtet.")]
         public string HomeAddress { get; set; }
 
-        [Required(ErrorMessage = "Postnummer krävs")]
+        [Required(ErrorMessage = "Postnummer krävs.")]
+        [StringLength(6, ErrorMessage = "Max 6 tecken tillåtet.")]
+        [RegularExpression(@"^[0-9 ]*$", ErrorMessage = "Endast siffror tillåtet.")]
         public string ZipCode { get; set; }
 
-        [Required(ErrorMessage = "Ort krävs")]
+        [Required(ErrorMessage = "Ort krävs.")]
+        [RegularExpression(@"^[a-zA-ZåäöÅÄÖéÉ ]*$", ErrorMessage = "Endast bokstäver tillåtet.")]
         public string City { get; set; }
 
-        [Required(ErrorMessage = "Användarnamn krävs")]
+        [Required(ErrorMessage = "Användarnamn krävs.")]
+        [RegularExpression(@"^[a-zA-ZåäöÅÄÖéÉ0-9._-]*$", ErrorMessage = "Innehåller otillåtna tecken.")]
         public string UserName { get; set; }
 
-        [Required(ErrorMessage = "Lösenord krävs")]
+        [Required(ErrorMessage = "Lösenord krävs.")]
         [DataType(DataType.Password)]
         public string Password { get; set; }
 
-        [Required(ErrorMessage = "Bekräfta lösenordet")]
+        [Required(ErrorMessage = "Bekräfta lösenordet.")]
         [DataType(DataType.Password)]
         [Compare("Password", ErrorMessage = "Lösenorden matchar inte.")]
         public string ConfirmPassword { get; set; }
