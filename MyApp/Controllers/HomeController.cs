33b4aeddb6f22b37d5195ba4a01e4c8656b3f48d
liamJonsson <limme98@gmail.com>
2026-01-09 19:56:22 +0100

    Revert "fixedVersionOfSearch"
    This reverts commit 0f78b42bd13e0b73441c7d8425d9f6815419c745.

--- MyApp/Controllers/HomeController.cs
+++ MyApp/Controllers/HomeController.cs
@@ -37,18 +37,10 @@ namespace MyApp.Controllers
 
             if (!string.IsNullOrWhiteSpace(query))
             {
-                // Dela upp söksträngen där mellanslag förekommer, tar bort tomma ord, alltså mellanslag"
-                var terms = query
-                    .Split(' ', StringSplitOptions.RemoveEmptyEntries);
-
-                foreach (var term in terms)
-                {
-                    users = users.Where(u =>
-                        u.Name.Contains(term) ||
-                        (u.Skills != null && u.Skills.Contains(term))
-                    );
-                }
+                //Här byggs frågan på med ett where villkor
+                users = users.Where(u => u.Name.Contains(query));
             }
+            //Och här körs faktiskt databasen, inte innan alls
             return View("Index", users.ToList());
         }
 
